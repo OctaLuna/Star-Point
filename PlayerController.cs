6c54f20e0d2e8d314be008066cdf670ffa3b2092
Marco <marco.octavio.luna@gmail.com>
2021-02-28 08:23:46 -0400

    Commit 1-Mr.Robot, se programo las animaciones del player, se uso el script de PlayerController

--- PlayerController.cs
+++ PlayerController.cs
@@ -17,11 +17,22 @@ public class PlayerController : MonoBehaviour
     //Esto es para declarar el suelo donde el Player pueda saltar
     bool isGrounded = false;
 
+    //ANIMACIONES
+    Animator Animator;
+    SpriteRenderer SpriteRenderer;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         //Estamos declarando el RigidBody del player
         Rigidbody = GetComponent<Rigidbody2D>();
+        //Estamos declarando el Animator del player
+        Animator = GetComponent<Animator>();
+        //Estamos declarando el SpriteRenderer del player
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -31,13 +42,39 @@ public class PlayerController : MonoBehaviour
         if(isGrounded == true){
             Jump();
         }
+        
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //FUNCIONES//
     //Movimiento
     void Move(){
         //Esto es para podernos mover de en eje horizontal
         Rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * PlayerSpeed, Rigidbody.velocity.y);
+        if(Input.GetAxis("Horizontal") == 0){
+            Animator.SetBool("isWalking", false);
+        }
+        else if(Input.GetAxis("Horizontal") < 0){
+            Animator.SetBool("isWalking", true);
+            SpriteRenderer.flipX = true;
+        }
+        else if(Input.GetAxis("Horizontal") > 0){
+            Animator.SetBool("isWalking", true);
+            SpriteRenderer.flipX = false;
+        }
     }
     //Salto
     void Jump(){
@@ -46,6 +83,7 @@ public class PlayerController : MonoBehaviour
             Rigidbody.AddForce(Vector2.up * JumpForce);
             Debug.Log("JUMP");
             isGrounded = false;
+            Animator.SetBool("isGrounded", false);
         }    
     }
 
@@ -53,6 +91,7 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground")){
             isGrounded = true;
             Debug.Log("isGround");
+            Animator.SetBool("isGrounded", true);
         }
     }
 }
