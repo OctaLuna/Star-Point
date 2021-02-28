6d0f8c12c506f76ebdb6a1034c1edc4b7ad7408f
Marco <marco.octavio.luna@gmail.com>
2021-02-28 12:12:59 -0400

    Commit 4-Mr.Robot, add DontDestroyOnLoad, se programo el script DontDestroyOnLoad, que sirve para que no haya 2 mismos objetos en una escena, en este caso se usa para la camara

---
+++ DontDestroyOnLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Esto es un controlador si en las transciones de escenas hay 2 camaras
+        //Esto sucedera cuando no haya 2 player
+        if(!PlayerController.playerCreated){
+            //Esto es para que en la transcion no se elimine el camara de que hace la transcicion
+            DontDestroyOnLoad(this.transform.gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+
+}
