97de63c7b31047593d2e0022bbf9337cb24d7f43
Marco <marco.octavio.luna@gmail.com>
2021-03-02 16:06:10 -0400

    Commit 6, add CourseOfScene y TortugaController, se programo la trasncicion entre escenas

---
+++ CourseOfScenes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;              //Esto es el controlador de escenas
+
+public class CourseOfScenes : MonoBehaviour
+{
+    public void ChangeSceneTo(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
