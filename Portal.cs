5854e5bb2e8dff55b6e215cc994c06fc945d1088
Marco <marco.octavio.luna@gmail.com>
2021-03-04 16:38:13 -0400

    Commit 8, add Portal, se programo un script para el player, cuando collisione con el portal ira a otra escena

---
+++ Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //Esto es para manejar las escenas en el juego
+
+public class Portal : MonoBehaviour
+{
+    //VARIABLES//
+    //Portal
+    bool transcition = false;
+    public string newPlaceName = "Pon el nombre de la escena a la que quieres ir";
+
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.CompareTag("Player")){
+            SceneManager.LoadScene(newPlaceName);
+        }
+    }
+}
